898bc534ed5d20d943871d53fa4d289c6fe6cd5e
Rajesh <rajhseg@gmail.com>
2026-08-19 09:35:29 +0530

    run ssr version in dev mode
    npm run serve:ssr:angularcontrols

--- Angular controls/server.ts
+++ Angular controls/server.ts
@@ -20,6 +20,9 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
+  server.use('/angular', express.static(browserDistFolder, {
+    maxAge: '1y'
+  }));
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
